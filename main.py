c24111ee1fa76ad2414b749b8f35d0e455519a66
Raphael R Santos <raphaelrsantos@gmail.com>
2025-02-14 21:46:30 -0300

    feat: resumo de PDF utilizando OpenAI
    - Atualização da descrição da API;
    - inclusão da função de resumo utilizando a OpenAI;

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from utils import (
     obter_logger_e_configuracao, 
     converter_pdf_para_texto_pyPDF2, 
-    resumir_pdf_groq
+    resumir_pdf_groq,
+    resumir_pdf_openai
 )
 
 
@@ -11,7 +12,8 @@ logger = obter_logger_e_configuracao()
 
 description = """
     API desenvolvida para o trabalho final da disciplina de API.
-    Trabalho desenvolvido por Guilherme Lemes, Raphael Rodrigues e Thiago Santos."""
+    Desenvolvido por Guilherme Lemes, Raphael Rodrigues e Thiago Santos, 
+    não obstante o código estar concentrado em uma única conta no github."""
 
 app = FastAPI(
     title="API - Projeto Final",
@@ -19,20 +21,18 @@ app = FastAPI(
     version="0.1",
     terms_of_service="http://example.com/terms/",
     contact={
-        "name": "Guilherme Lemes, Raphael Rodrigues, Thiago Santos",
+        "name": "Raphael Rodrigues",
         "url": "http://github.com/raphaelrsantos/",
-        "email": "grupo14@EMAIL.COM",
-    },
-    license_info={
-        "name": "Apache 2.0",
-        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        "email": "raphaelrsantos@gmail.com",
     },  
 )
 
 @app.post(
     "/v1/converter_pdf_para_texto_pyPDF2", 
     summary="Converte um PDF para texto",
-    description="Extrai o texto de um arquivo PDF usando PyPDF2."
+    description="Extrai o texto de um arquivo PDF usando a biblioteca PyPDF2 do Python.",
+    tags=["Conversão de arquivos"],
+    
 )
 def converter_pdf(caminho_pdf: str):
     texto_extraido = converter_pdf_para_texto_pyPDF2(caminho_pdf)
@@ -42,8 +42,20 @@ def converter_pdf(caminho_pdf: str):
 @app.post(
     "/v1/pdf_resumo_groq", 
     summary="Gera um resumo do PDF utilizando Groq como LLM.",
-    description="Extrai o texto do PDF e produz um resumo estruturado em tópicos utilizando a Groq como LLM."
+    description="Extrai o texto do PDF e produz um resumo estruturado em tópicos utilizando a Groq como LLM.",
+    tags=["Manipulação de PDFs"],
 )
 def resumir_pdf(caminho_pdf: str):
     resultado = resumir_pdf_groq(caminho_pdf)
+    return resultado
+
+
+@app.post(
+    "/v1/pdf_resumo_openai", 
+    summary="Gera um resumo do PDF utilizando a OpenAI como LLM.",
+    description="Extrai o texto do PDF e produz um resumo estruturado em tópicos utilizando a OpenaAI como LLM.",
+    tags=["Manipulação de PDFs"],
+)
+def resumir_pdf(caminho_pdf: str):
+    resultado = resumir_pdf_openai(caminho_pdf)
     return resultado
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 from groq import Groq, APIStatusError
 import PyPDF2  # biblioteca para manipulação de PDFs
-
+import openai
 
 load_dotenv()
 
@@ -19,6 +19,17 @@ def obter_logger_e_configuracao():
     )
     logger = logging.getLogger("fastapi")
     return logger
+
+
+def limpar_json_formatado(json_text):
+    """
+    Remove caracteres de markdown como ```json e ``` do início e fim do texto.
+    """
+    if json_text.startswith("```json"):
+        json_text = json_text[7:]  # Remove o ```json inicial
+    if json_text.endswith("```"):
+        json_text = json_text[:-3]  # Remove o ``` final
+    return json_text.strip()
 
 
 def converter_pdf_para_texto_pyPDF2(caminho_pdf: str) -> str:
@@ -81,3 +92,68 @@ def resumir_pdf_groq(caminho_pdf: str) -> str:
     
     resumo = resposta.choices[0].message.content
     return {"resumo": resumo}
+
+
+def acessar_api_openai(content: str, prompt: str, modelo: str) -> str:
+    # Configurar a chave da API da OpenAI
+    api_key = os.getenv("OPENAI_API_KEY")
+    if not api_key:
+        raise ValueError("A chave da API 'OPENAI_API_KEY' não foi encontrada. Verifique o arquivo .env.")
+
+    # Inicializar o cliente da OpenAI
+    client = openai.OpenAI(api_key=api_key)
+    
+    # Histórico da conversa
+    conversation_history = [
+        {"role": "system", "content": f"{content}"}
+    ]
+
+    # Adicionar a entrada ao histórico
+    conversation_history.append({"role": "user", "content": prompt})
+    
+    try:
+        completion = client.chat.completions.create(
+            model=f"{modelo}",
+            messages=conversation_history
+        )
+    
+        # Extrair a resposta do modelo
+        gpt_response = completion.choices[0].message.content
+        
+        # Limpar o JSON formatado
+        cleaned_json = limpar_json_formatado(gpt_response)
+        
+        return cleaned_json
+        
+    except APIStatusError as e:
+        error_str = str(e)
+        if "rate_limit_exceeded" in error_str:
+            return ("Erro na conversão para XML: O request excedeu o limite de tokens por minuto. "
+                    "Por favor, reduza o tamanho da mensagem e tente novamente.")
+        else:
+            return f"Erro na conversão para XML: {e}"
+
+
+def resumir_pdf_openai(caminho_pdf: str) -> str:
+    """
+    Converte um PDF para TXT estruturado com tags XML utilizando uma LLM (OPenAI).
+
+    Args:
+        caminho_pdf (str): O caminho para o arquivo PDF.
+    Returns:
+        str: O texto convertido para o padrão XML.
+    """
+    # Extrai o texto bruto do PDF
+    texto_pdf = converter_pdf_para_texto_pyPDF2(caminho_pdf)
+    
+    modelo_user = "gpt-4o-mini" # Modelos da OpenAI: gpt-4o, gpt-4o-turbo, gpt-4o-mini
+    instrucao_user = "Você é um assistente para resumir longos textos em PDF."
+    prompt_user = ("A partir do conteúdo txt extraído do PDF, crie um resumo esquemático e o mais didático possível. Ao final do resumo, "
+        "O resumo deve ser em português, claro, objetivo e facilitar a compreensão para o usuário final. Coloque quebra de linhas no resumo. "
+        "Não explicitar a palavra resumo no corpo da resposta\n\n"
+        f"Texto extraído:\n{texto_pdf}"
+        ""
+    )
+    
+    resultado = acessar_api_openai(content=instrucao_user, prompt=prompt_user, modelo=modelo_user)
+    return {"resultado": resultado}
